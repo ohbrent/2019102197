20aa4ca22dfd02d2a15366a9a04e40251255a60c
brent oh <brent912@naver.com>
2022-04-08 10:31:54 +0900

    구구단 파일 생성

---
+++ 구구단.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+//2019102197 오세헌
+
+int main()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 9; j++)
+		{
+			cout << i + 1 << " * " << j + 1 << " = " << (i + 1) * (j + 1);
+		}
+	}
+
+	return 0;
+}
